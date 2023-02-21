60478e3b8b3a7ba2843eb059291fc1c2ef5392b3
Juan Alvarado <jujalvarado@est.istg.edu.ec>
2023-02-21 17:39:24 -0500

    cambios por juan

--- kinder.sql
+++ kinder.sql
@@ -1,4 +1,15 @@
 
+/*Script: construccion de base de datos "kinder"*/
+
+/*Integrantes
+ * 
+ * 
+ * 
+ * 
+ * Juan Jose Alvarado Piza 
+ */
+
+
 
 drop database if exists kinder;
 create database kinder;
@@ -21,29 +32,77 @@ drop table if exists administradores;
 create table administradores (
   id int not null auto_increment,
   cedula varchar(20) not null,
-  nombre varchar(50) not null,
-  apellido varchar(50) not null,
+  nombres varchar(50) not null,
+  apellidos varchar(50) not null,
   genero enum('m','f') not null,
   email varchar(100) not null,
   telefono varchar(20) not null,
-  id_usuario int not null,
+  id_usuario int,
   primary key (id),
   foreign key (id_usuario) references usuarios(id)
 );
 
 
+/*-------------------------------------------------------------------------------------------
+ 						Tablas relacionadas con representantes 
+ -------------------------------------------------------------------------------------------*/
+
 drop table if exists representantes;
 create table representantes (
   id int not null auto_increment,
   cedula varchar(20) not null,
-  nombre varchar(50) not null,
-  apellido varchar(50) not null,
+  nombres varchar(50) not null,
+  apellidos varchar(50) not null,
+  fecha_nacimiento date not null,
   genero enum('m','f') not null,
   email varchar(100) not null,
   telefono varchar(20) not null,
-  id_usuario int not null,
+  id_usuario int,
   primary key (id),
   foreign key (id_usuario) references usuarios(id)
+);
+
+
+drop table if exists informacion_representante;
+create table informacion_representante (
+  id int not null auto_increment,
+  id_representante int not null,
+  
+  
+  /*informacion adicional */
+  parentesco varchar(30)not null,
+  estado_civil varchar(20) not null,
+  etnia varchar(30) not null,
+  nivel_formacion varchar (100)not null,
+  ingresos decimal(10,2) not null,
+  
+  
+  /*de donde proviene  */
+  pais_nacionalidad varchar(50) not null,
+  
+  pais_nacimiento varchar(50) not null,
+  provincia_nacimiento varchar(50) not null,
+  canton_nacimiento varchar(50) not null,
+  
+  pais_residencia varchar(50) not null,
+  provincia_residencia varchar(50) not null,
+  canton_residencia varchar(50) not null,
+  
+  direccion_domicilio varchar(120)not null,
+  
+  
+  /* salud*/
+  discapacidad boolean not null,
+  porcentaje_disc int ,
+  tipo_disc varchar(50),
+  nro_carnet int,
+  enfermedad varchar(100),
+  
+  tipo_sangre varchar(10)not null,
+  
+  
+  primary key (id),
+  foreign key (id_representante) references representantes(id)
 );
 
 
@@ -59,18 +118,64 @@ create table documentos_representante (
 );
 
 
+/*-------------------------------------------------------------------------------------------
+							Tablas relacionadas con estudiantes 
+-------------------------------------------------------------------------------------------*/
+
 drop table if exists estudiantes;
 create table estudiantes (
   id int not null auto_increment,
   cedula varchar(20) not null,
-  nombre varchar(50) not null,
-  apellido varchar(50) not null,
+  nombres varchar(50) not null,
+  apellidos varchar(50) not null,
   genero enum('m','f') not null,
   fecha_nacimiento date not null,
   id_representante int not null,
   primary key (id),
   foreign key (id_representante) references representantes(id)
 );
+
+
+
+drop table if exists informacion_estudiante;
+create table informacion_estudiante (
+  id int not null auto_increment,
+  id_estudiante int not null,
+  
+  
+  /*informacion adicional */
+  estado_civil varchar(20) not null,
+  etnia varchar(30) not null,
+  hobbie varchar(100),
+  
+  /*de donde proviene  */
+  pais_nacionalidad varchar(50) not null,
+  
+  pais_nacimiento varchar(50) not null,
+  provincia_nacimiento varchar(50) not null,
+  canton_nacimiento varchar(50) not null,
+  
+  pais_residencia varchar(50) not null,
+  provincia_residencia varchar(50) not null,
+  canton_residencia varchar(50) not null,
+  
+  direccion_domicilio varchar(120)not null,
+  
+  
+  /* salud*/
+  discapacidad boolean not null,
+  porcentaje_disc int ,
+  tipo_disc varchar(50),
+  nro_carnet int,
+  enfermedad varchar(100),
+  
+  tipo_sangre varchar(10)not null,
+  
+  
+  primary key (id),
+  foreign key (id_estudiante) references estudiantes(id)
+);
+
 
 
 drop table if exists documentos_estudiante;
@@ -85,30 +190,73 @@ create table documentos_estudiante (
 );
 
 
-drop table if exists asistencias;
-create table asistencias (
-  id int auto_increment,
-  id_estudiante int not null,
-  fecha_asistencia date not null,
-  asistio tinyint(1) not null,
-  primary key (id),
-  foreign key (id_estudiante) references estudiantes(id)
-);
 
+
+/*-------------------------------------------------------------------------------------------
+ 							Tablas relacionadas con maestros 
+ -------------------------------------------------------------------------------------------*/
 
 drop table if exists maestros;
 create table maestros (
   id int not null auto_increment,
   cedula varchar(20) not null,
-  nombre varchar(50) not null,
-  apellido varchar(50) not null,
+  nombres varchar(50) not null,
+  apellidos varchar(50) not null,
   genero enum('m','f') not null,
   email varchar(100) not null,
   telefono varchar(20) not null,
-  id_usuario int not null,
+  id_usuario int,
   primary key (id),
   foreign key (id_usuario) references usuarios(id)
 );
+
+
+drop table if exists informacion_maestro;
+create table informacion_maestro (
+  id int not null auto_increment,
+  id_maestro int not null,
+  
+  
+  /*informacion adicional */
+  estado_civil varchar(20) not null,
+  etnia varchar(30) not null,
+  
+  nivel_formacion varchar (100)not null,
+  especialidad varchar (100)not null,
+  experiencia_laboral int not null,
+  ingresos decimal(10,2) not null,
+  
+  prueba_personalidad decimal(5,2)not null,
+  
+  
+  /*de donde proviene  */
+  pais_nacionalidad varchar(50) not null,
+  
+  pais_nacimiento varchar(50) not null,
+  provincia_nacimiento varchar(50) not null,
+  canton_nacimiento varchar(50) not null,
+  
+  pais_residencia varchar(50) not null,
+  provincia_residencia varchar(50) not null,
+  canton_residencia varchar(50) not null,
+  
+  direccion_domicilio varchar(120)not null,
+  
+  
+  /* salud*/
+  discapacidad boolean not null,
+  porcentaje_disc int ,
+  tipo_disc varchar(50),
+  nro_carnet int,
+  enfermedad varchar(100),
+  
+  tipo_sangre varchar(10)not null,
+  
+  
+  primary key (id),
+  foreign key (id_maestro) references maestros(id)
+);
+
 
 
 drop table if exists documentos_maestro;
@@ -123,6 +271,13 @@ create table documentos_maestro (
 );
 
 
+
+/*------------------------------------------------------------------------------------------- 
+	 					Tablas relacionadas con el curso y matricula
+					(asignacion del maestro y periodo lectivo al curso, 
+	 								datos de matricula)
+ --------------------------------------------------------------------------------------------*/
+
 drop table if exists cursos;
 create table cursos (
   id int not null auto_increment,
@@ -132,14 +287,28 @@ create table cursos (
 );
 
 
+drop table if exists periodo_lectivo;
+create table periodo_lectivo (
+  id int not null auto_increment,
+  nombre varchar(50) not null,
+  descripción text not null,
+  desde datetime not null,
+  hasta datetime not null, 
+  primary key (id)
+);
+
+
 drop table if exists curso_maestro;
 create table curso_maestro(
   id int not null auto_increment,
   id_curso int not null,
   id_maestro int not null,
+  id_periodo_lectivo int not null,
+  vigente boolean not null,
   primary key (id),
   foreign key (id_curso) references cursos(id),
-  foreign key (id_maestro) references maestros(id)
+  foreign key (id_maestro) references maestros(id),
+  foreign key (id_periodo_lectivo) references periodo_lectivo(id)
 );
 
 
@@ -148,11 +317,13 @@ create table matriculas (
   id int not null,
   id_estudiante int not null,
   id_curso int not null,
-  fecha_matricula date not null,
+  id_periodo_lectivo int not null,
+  fecha_matricula datetime not null,
   vigente boolean not null,
   primary key (id),
   foreign key (id_estudiante) references estudiantes(id),
-  foreign key (id_curso) references cursos(id)
+  foreign key (id_curso) references cursos(id),
+  foreign key (id_periodo_lectivo)references periodo_lectivo(id)
 );
 
 
@@ -166,6 +337,15 @@ create table documentos_matricula (
     primary key (id),
     foreign key (id_matricula) references matriculas(id)
 );
+
+
+
+
+/*-------------------------------------------------------------------------------------------
+						Tablas relacionadas con los procesos del curso 
+					(materias, asignaturas, como se divide el periodo lectivo,
+							tareas,calificaciones, asistencia)
+ *------------------------------------------------------------------------------------------- */
 
 
 drop table if exists materias;
@@ -188,12 +368,26 @@ create table asignaturas (
 );
 
 
-drop table if exists parcial_quimestre;
-create table parcial_quimestre (
+
+drop table if exists quimestre;
+create table quimestre (
   id int not null auto_increment,
   nombre varchar(50) not null,
   descripcion text not null,
-  primary key (id)
+  id_periodo_lectivo int not null,
+  primary key (id),
+  foreign key (id_periodo_lectivo) references periodo_lectivo(id)
+);
+
+
+drop table if exists parcial;
+create table parcial (
+  id int not null auto_increment,
+  nombre varchar(50) not null,
+  descripcion text not null,
+  id_quimestre int not null,
+  primary key (id),
+  foreign key (id_quimestre) references quimestre(id)
 );
 
 
@@ -201,14 +395,30 @@ drop table if exists tareas;
 create table tareas (
   id int auto_increment,
   id_asignatura int not null,
-  id_parcial_quimestre int not null,
+  id_parcial int not null,
   titulo varchar(255) not null,
   descripcion text,
   fecha timestamp default current_timestamp,
   primary key (id),
   foreign key (id_asignatura) references asignaturas(id),
-  foreign key (id_parcial_quimestre) references parcial_quimestre(id)
+  foreign key (id_parcial) references parcial(id)
 );
+
+
+drop table if exists asistencias;
+create table asistencias (
+  id int auto_increment,
+  id_estudiante int not null,
+  id_parcial int not null,
+  id_asignatura int not null,
+  fecha_asistencia date not null,
+  asistio boolean not null,
+  primary key (id),
+  foreign key (id_estudiante) references estudiantes(id),
+  foreign key (id_parcial) references parcial(id),
+  foreign key (id_asignatura) references asignaturas(id)
+);
+
 
 
 drop table if exists calificaciones;
@@ -223,6 +433,12 @@ create table calificaciones (
   foreign key (id_tarea) references tareas(id)
 );
 
+
+
+
+/*----------------------------------------------------------------------------------
+ 						Tablas relacionadas con el sistema de facturas/pagos 
+ -----------------------------------------------------------------------------------*/
 
 drop table if exists tipo_factura;
 create table tipo_factura (
@@ -251,12 +467,8 @@ create table facturas (
 
 
 
--- agregar tablas de informacion adicional de estudiante, representante, maestro
 
-
--- show tables
-
--- describe database 
+--show tables;
 
 
 
